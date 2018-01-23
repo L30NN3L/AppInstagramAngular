0c58f0f1628b86a56cc2a6dc6577836ede8d1553
Hugo Leonel <hugoleone.net@gmail.com>
2018-01-23 18:34:31 -0200

    Animação Pronta

--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, trigger, state, style, transition, animate } from '@angular/core';
 
+import { Imagem } from './imagem.model';
+
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
@@ -20,13 +22,46 @@ export class BannerComponent implements OnInit {
 
   public estado = 'escondido';
 
+  public imagens: Imagem[] = [
+    { estado: 'visivel' , url: '/assets/banner-acesso/img_1.png' },
+    { estado: 'escondido' , url: '/assets/banner-acesso/img_2.png' },
+    { estado: 'escondido' , url: '/assets/banner-acesso/img_3.png' },
+    { estado: 'escondido' , url: '/assets/banner-acesso/img_4.png' },
+    { estado: 'escondido' , url: '/assets/banner-acesso/img_5.png' }
+  ];
+
   constructor() { }
 
   ngOnInit() {
+    // console.log(this.imagens);
+    setTimeout( () => this.logicaRotacao(), 3000);
   }
 
-  public toggleEstado(): void {
-    this.estado = this.estado === 'visivel' ? 'escondido' : 'visivel';
+  public logicaRotacao(): void {
+  //  console.log(this.imagens);
+
+    // auxilia na exibição na imagem seguinte
+    let idx: number;
+
+    // ocultar imagem
+    for (let i = 0; i <= 4; i++) {
+      if (this.imagens[i].estado === 'visivel') {
+        this.imagens[i].estado = 'escondido';
+
+        idx = ((i === 4) ? 0 : i + 1);
+
+        break;
+      }
+    }
+
+    // exibir a próxima imagem
+    this.imagens[idx].estado = 'visivel';
+
+    setTimeout( () => this.logicaRotacao(), 3000);
   }
+
+  /* public toggleEstado(): void {
+    this.estado = this.estado === 'visivel' ? 'escondido' : 'visivel';
+  } */
 
 }
