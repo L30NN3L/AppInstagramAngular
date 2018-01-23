3b4bbfe3ef6aead90df47e7d19208b6fbaa10c1c
Hugo Leonel <hugoleone.net@gmail.com>
2018-01-23 14:21:27 -0200

    Projeto Angular Instagram

---
+++ src/app/acesso/banner/banner.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, trigger, state, style, transition, animate } from '@angular/core';
+
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.css'],
+  animations: [
+    trigger('banner', [
+      state('escondido', style({
+        opacity: 0
+      })),
+      state('visivel', style({
+        opacity: 1
+      })),
+      transition('escondido <=> visivel', animate('1s ease-in') )
+    ])
+  ]
+})
+export class BannerComponent implements OnInit {
+
+  public estado = 'escondido';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public toggleEstado(): void {
+    this.estado = this.estado === 'visivel' ? 'escondido' : 'visivel';
+  }
+
+}
